7b98afb5e37c6484c50e402d5bd036a9cc5fec02
Sojo506 <fabiansojowork@gmail.com>
2023-02-19 13:11:47 -0600

    Validation, and getting data

--- js/app.js
+++ js/app.js
@@ -14,8 +14,45 @@ function safeClient() {
 
   // validate is there are empty fields
   const emptyFields = [table, hour].some((field) => field === "");
-  console.log("🚀 ~ file: app.js:17 ~ safeClient ~ emptyFields", emptyFields)
 
-  if (emptyFields) console.log("at least an empty field");
-  else console.log("good");
+  if (emptyFields) {
+    // verify is already there is an alert
+    const checkAlert = document.querySelector(".invalid-feedback");
+    if (checkAlert) return;
+
+    const alert = document.createElement("DIV");
+    alert.classList.add("invalid-feedback", "d-block", "text-center");
+    alert.textContent = "All fields are required";
+
+    // add alert
+    const modal = document.querySelector(".modal-body form");
+    modal.appendChild(alert);
+
+    // remove alert
+    setTimeout(() => {
+      // modal.removeChild(modal.lastChild);
+      alert.remove();
+    }, 3000);
+
+    return;
+  }
+
+  // take data
+  client = { ...client, table, hour };
+
+  // hide modal
+  const formModal = document.querySelector("#form");
+  const bootstrapModal = bootstrap.Modal.getInstance(formModal);
+
+  bootstrapModal.hide();
+  client = { ...client, table: "", hour: "" };
+
+  // show Secctions
+  showSecctions()
+}
+
+function showSecctions() {
+  const hideSecctions = document.querySelectorAll('.d-none')
+  hideSecctions.forEach(secction => secction.classList.remove('d-none'))
+
 }
